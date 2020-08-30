9aff094f018e9be494268dc714d2b1e30ce1ea5a
Christian Martins <christianrmmartins@gmail.com>
2020-08-30 18:43:05 -0300

    FIX: Init litho test.

--- app/src/main/java/br/com/christianmartins/declarativeviews/MainActivity.kt
+++ app/src/main/java/br/com/christianmartins/declarativeviews/MainActivity.kt
@@ -2,10 +2,21 @@ package br.com.christianmartins.declarativeviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.LithoView
+import com.facebook.litho.widget.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        val componentContext = ComponentContext(this)
+        val component = Text.create(componentContext)
+            .text("- Test -")
+            .textSizeDip(50f)
+            .build()
+
+        setContentView(LithoView.create(componentContext, component))
     }
 }
